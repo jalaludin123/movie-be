2c5b6d9bed1c699cb6f80057263a3a2ccba9a2a8
jalaludin <jalaludinscoter12@gmail.com>
2023-08-27 13:59:14 +0700

    first commit in branch dev

--- src/user/dto/create-input-user.ts
+++ src/user/dto/create-input-user.ts
@@ -1,14 +1,19 @@
 import { InputType, Field } from '@nestjs/graphql';
+import { IsNotEmpty } from 'class-validator';
 import Role from 'src/data_enum/role/roles';
 
 @InputType()
 export class CreateUserInput {
+
+  @IsNotEmpty()
   @Field()
   email: string;
 
+  @IsNotEmpty()
   @Field()
   name: string;
 
+  @IsNotEmpty()
   @Field({ nullable: true })
   password: string;
 
